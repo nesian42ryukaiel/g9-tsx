cec04cf2fd2fdc9c68396b66d94b887d5a07a230
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-04-09 14:05:12 +0900

    new configureStoreAsync apparently complete no idea if return Promise.reslove(store) works, though...

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import axios from "axios";
 import "./css/index.css";
 import { resolve } from "node:path/win32";
 
-const configureStoreAsync2 = async () => {
+const configureStoreAsync = async () => {
   const initColorValue = localStorage.getItem("colormode");
   const colorValue = initColorValue !== null ? initColorValue : "light";
   console.log("[DEBUG] local storage color mode:", initColorValue);
@@ -26,50 +26,13 @@ const configureStoreAsync2 = async () => {
     const response = await axios.get(pServerLink + "/" + artdata);
     initialState.pages.articles = JSON.parse(JSON.stringify(response.data));
     const store = createStore(rootReducer, initialState);
-    resolve(store);
+    return Promise.resolve(store);
   } catch (error) {
     console.log(error);
     const store = createStore(rootReducer, initialState);
-    resolve(store);
+    return Promise.resolve(store);
   }
 };
-
-function configureStoreAsync() {
-  return new Promise((resolve) => {
-    const initColorValue = localStorage.getItem("colormode");
-    const colorValue = initColorValue !== null ? initColorValue : "light";
-    console.log("[DEBUG] local storage color mode:", initColorValue);
-    let initialState = {
-      pages: {
-        currentPage: "index",
-        articles: [],
-      },
-      media: {
-        mediaColorMode: colorValue,
-      },
-    };
-    try {
-      axios
-        .get(pServerLink + "/" + artdata)
-        .then(function (response) {
-          initialState.pages.articles = JSON.parse(
-            JSON.stringify(response.data)
-          );
-        })
-        .catch(function (error) {
-          console.log(error);
-        })
-        .finally(function () {
-          const store = createStore(rootReducer, initialState);
-          resolve(store);
-        });
-    } catch (error) {
-      const store = createStore(rootReducer, initialState);
-      console.log(store.getState());
-      resolve(store);
-    }
-  });
-}
 
 configureStoreAsync().then((result: any) => {
   const store = result;
@@ -87,3 +50,40 @@ configureStoreAsync().then((result: any) => {
     document.getElementById("root")
   );
 });
+
+// function configureStoreAsync1() {
+//   return new Promise((resolve) => {
+//     const initColorValue = localStorage.getItem("colormode");
+//     const colorValue = initColorValue !== null ? initColorValue : "light";
+//     console.log("[DEBUG] local storage color mode:", initColorValue);
+//     let initialState = {
+//       pages: {
+//         currentPage: "index",
+//         articles: [],
+//       },
+//       media: {
+//         mediaColorMode: colorValue,
+//       },
+//     };
+//     try {
+//       axios
+//         .get(pServerLink + "/" + artdata)
+//         .then(function (response) {
+//           initialState.pages.articles = JSON.parse(
+//             JSON.stringify(response.data)
+//           );
+//         })
+//         .catch(function (error) {
+//           console.log(error);
+//         })
+//         .finally(function () {
+//           const store = createStore(rootReducer, initialState);
+//           resolve(store);
+//         });
+//     } catch (error) {
+//       const store = createStore(rootReducer, initialState);
+//       console.log(store.getState());
+//       resolve(store);
+//     }
+//   });
+// }

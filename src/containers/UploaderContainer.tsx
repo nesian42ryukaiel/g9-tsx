e158bfec0774b9344b2250ca9573963b1ff45ca0
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-04-10 11:07:29 +0900

    unused import

--- src/containers/UploaderContainer.tsx
+++ src/containers/UploaderContainer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Uploader from "../components/Uploader";
 import { movePage, uploadPage } from "../modules/pages";
